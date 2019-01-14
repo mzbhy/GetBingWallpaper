3188391968f26b5ec9a5ab11ce8d49eab4e741a7
Xiang LI <120411@bit.edu.cn>
2019-01-14 15:43:08 +0800

    python version

---
+++ GetBingWallpaperPy.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+"""Download Bing everyday wallpaper.
+Usage:
+	GetBingWallpaperPy [-cps]
+	GetBingWallpaperPy [-cps] PATH
+Options:
+	-h --help	
+	-c		显示版权信息
+	-p		设置壁纸前预览今日壁纸
+	-s		直接设置壁纸，无需确认
+Example:
+	GetBingWallpaperPy
+	GetBingWallpaperPy -c
+	GetBingWallpaperPy -cps
+	GetBingWallpaperPy -cp D:\\temp
+"""
+
+from urllib.request import urlopen, urlretrieve
+from xml.dom import minidom
+from PIL import Image
+from docopt import docopt
+import win32api, win32gui, win32con
+import sys,io,os
+import threading
+
+# 改变标准输出的默认编码 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
+
+
+class BingInfo(object):
+	"""Infomation about Bing Wallpaer."""
+	def __init__(self):
+		super(BingInfo, self).__init__()
+		_copyRight = ""
+		_url = ""
+		_fullDir = ""
+		_saveDir = ""
+		_fullDirBmp = ""
+
+def downloadWallpaper(todayWall):
+	"""Download the Bing Wallpaper using xml api."""
+	try:
+		wallResponse = urlopen('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1')
+		wallXML = minidom.parse(wallResponse)
+		wallURL = wallXML.getElementsByTagName("url")
+		todayWall._url = wallURL[0].firstChild.nodeValue
+		wallCopyRight = wallXML.getElementsByTagName("copyright")
+		todayWall._copyRight = wallCopyRight[0].firstChild.nodeValue
+		todayWall._fullDir = todayWall._saveDir + '\\' + todayWall._url[todayWall._url.rindex("/") + 1:]
+		todayWall._fullDirBmp = todayWall._fullDir.replace('jpg','bmp')	
+		urlretrieve('http://cn.bing.com' + todayWall._url, todayWall._fullDir)
+	except Exception as e:
+		print("Image download error! Please check the internet connect!")
+		return False
+
+	try:
+		Image.open(todayWall._fullDir).save(todayWall._fullDirBmp)
+		os.remove(todayWall._fullDir)
+		return True
+	except Exception as e:
+		print(e.value)
+		return False
+	
+
+def setWallpaper(picPath):
+	'''Set the wallpaper in Windows system.'''
+	# cmd = 'REG ADD \"HKCU\Control Panel\Desktop\" /v Wallpaper /t REG_SZ /d \"%s\" /f' %picPath
+	# # cmd = 'REG ADD \"HKCU\Control Panel\Desktop\" /v Wallpaper /t REG_SZ /d \" D:\1.bmp \" /f' 
+	# os.system(cmd)
+	# os.system('rundll32.exe user32.dll, UpdatePerUserSystemParameters')
+	win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, picPath, 1+2) 
+
+class showImageThread(threading.Thread):
+	'''Extend the threading.Thread for showing image'''
+	def __init__(self, args):
+		threading.Thread.__init__(self)
+		self.args = args
+	def run(self):
+		im = Image.open(self.args[0]._fullDirBmp)
+		im.show()
+
+def main():
+	'''The main function.'''
+	arguments = docopt(__doc__)
+	todayWall = BingInfo()
+
+	filePath = arguments['<szFilePath>'].decode('GB2312')
+	if not filePath:
+		todayWall._saveDir = filePath
+	else:
+		todayWall._saveDir = os.path.dirname(os.path.realpath(__file__)) + "\images"
+
+	if not os.path.exists(todayWall._saveDir):
+		os.makedirs(todayWall._saveDir)
+	
+	if downloadWallpaper(todayWall):
+		if arguments['-c']:
+			print(todayWall._copyRight)
+		if arguments['-p']:
+			t = showImageThread(args=(todayWall,))
+			t.start()
+		if not arguments['-s']:
+			if input('Do you want to set this image as wallpaper? [y/n]:') == 'y':
+				isSet = True
+			else:
+				isSet = False
+		else:
+			isSet = True
+		if isSet:
+			setWallpaper(todayWall._fullDirBmp)
+		else:
+			print("No wallpaper is set!")
+		
+
+if __name__ == "__main__":
+	main()
--- README.md
+++ README.md
@@ -1,35 +1,52 @@
-# GetBingWallpaper
-利用C#编写，Visual Studio 2015编译。将Bing首页的背景图下载并设置为系统桌面，程序默认为托盘形式，双击可唤出主要窗体。
+# GetBingWallpaperPy
+公司发的笔记本无法使用之前C#编写的工具，具体原因还待查询，为了方便使用，写了一个简单的python版本。
+
+## 开发环境
+
+* python 3.5.4
+* sublime text 3
+* windows 7 64 bit
 
 ## 使用方法
 
-打开程序后，右击托盘选择“Preview Today‘s Wallpaper"或双击托盘，会出现加载有今日壁纸的主窗体：
+1. 安装依赖项（按需）
 
-![](http://ww1.sinaimg.cn/large/6486a686ly1fclnnz056lj20m80ci7ot)
+   ```bash
+   sudo pip install docopt
+   ```
 
-点击右上角√即可设置壁纸，点击×关闭窗体，鼠标放在i上可以获取该张壁纸的简单信息。
+2. 下载脚本
+
+   ```bash
+   git clone -b py https://github.com/lxalxy/GetBingWallpaper.git
+   cd GetBingWallpaper
+   ```
+
+3. 运行脚本设置壁纸（当路径存在特殊符号以及空格时，需要在路径外加双引号）。
+
+   ```bash
+   # 设置当日Bing壁纸，壁纸存放在工作路径下的image文件夹中
+   python GetBingWallpaperPy.py
+   # 设置当日Bing壁纸，输出版权信息，并预览壁纸，壁纸存放在指定目录
+   python GetBingWallpaperPy.py -cp D:\test
+   # 设置当日Bing壁纸，无需确认，直接设置
+   python GetBingWallpaperPy.py -s
+   ```
+
+## 参数说明
+
+* -h,--help        显示帮助菜单
+* -c                    显示版权信息
+* -p                   设置壁纸前预览今日壁纸
+* -s                    直接设置壁纸，无需确认
 
 ## 更新日志
 
-### 2016.8.20
+### 2018.1.3
 
-1. 利用Bing官方提供的XML格式API获取当日壁纸信息，下载并设置为桌面壁纸
-2. 实现托盘运行
+1. 完成初版
 
-### 2017.1.28
+### 2019.1.14
 
-1. 利用App.config文件进行设置的保存和读取
-2. 可选是否保留历史壁纸，并自由设定保留天数和保存路径
-3. 可选是否成功设置后自动退出
-4. 可选开机自启
-
-### 2017.1.29
-
-1. 权限问题解决，参考[该链接](https://www.oschina.net/question/1041591_221434)，将启动信息写入当前用户注册表中
-
-## To do
-
-1. 各国有可能具有不同的每日壁纸，目前已实现了不同国家的API的获取，但壁纸不会更新，原因待查
-2. 添加多屏不同分辨率支持
-
-~~添加win10锁屏修改功能~~（除了UWP应用，微软貌似封掉了API）
+1. 添加多线程处理，避免预览时终端无法操作
+2. 添加直接设置壁纸参数
